960effa98dfcfc7c46744850ef468992e9526bb3
padmed <ddarchinian@gmail.com>
2022-10-31 01:46:12 +0400

    Move manipulateInvisibleElement function to new EtchASketchMain class

--- logic.js
+++ logic.js
@@ -15,6 +15,7 @@ class EtchASketch {
         this.color = 'black';
     }
 
+    //creates parent - child div elements, takes grid form with css flexbox 
     fillPad = function (){
         for (let height = 1; height <= this.padSize; height++) {
             const div = document.createElement('div');
@@ -29,67 +30,54 @@ class EtchASketch {
         }
     }
 
+    //clears grid
     clearPad = function () {
         const toRemove = document.querySelectorAll('.parentDiv');
         toRemove.forEach((element) => element.remove());
     }
-
-    // interactiveInputRange  = function () {
-    //     EtchASketch.padSize = range.value;
-    //     EtchASketch.clearPad();
-    //     EtchASketch.fillPad();
-    // } 
-
-    // main = function () {
-    //     const range = document.getElementById('range');
-    //     range.addEventListener('input', this.interactiveInputRange);
-        
-    // }
 }
 
 
 
 class EtchASketchMain {
-    constructor (obj) {
+    constructor () {
         this.pad = new EtchASketch;
         this.inputRange = document.getElementById('range');
+        this.invisibleElement = document.getElementById('invisibleElement');
+        
         this.main();
     }
 
+    //sets propertie based on user selection, clears previous grid, fills new grid.
     interactiveInputRange = function () {
-        const inputRange = document.getElementById('range');
-        this.pad.padSize = inputRange.value;
+        this.pad.padSize = this.inputRange.value;
         this.pad.clearPad();
         this.pad.fillPad();
     }
 
-    main = function () {
-        this.inputRange.addEventListener('input', this.interactiveInputRange.bind(this))
+    // removes/adds invisible element based on window size
+    manipulateInvisibleElement = function () {
+        const width = document.documentElement.clientWidth;
+        const height = document.documentElement.clientHeight;
+
+        if (height <= 821 && width <= 1102 ) {
+            invisibleElement.classList.remove('invisibleElement');
+        } else {
+            invisibleElement.classList.add('invisibleElement');
+        }
     }
+        
 
-
-
+    main = function () {
+        this.inputRange.addEventListener('input', this.interactiveInputRange.bind(this)); // listens to input 'range'
+        invisibleElement.classList.add('invisibleElement'); //adds invisible element on program startup
+        window.addEventListener('resize', this.manipulateInvisibleElement) // listens for window resize
+    }
 }
 
 
 
 program = new EtchASketchMain()
 
-
-
-const invisibleElement = document.getElementById('invisibleElement');
-const removeInvisibleElement = function () {
-    const width = document.documentElement.clientWidth;
-    const height = document.documentElement.clientHeight;
-
-    if (height <= 821 && width <= 1102 ) {
-        invisibleElement.classList.remove('invisibleElement');
-    } else {
-        invisibleElement.classList.add('invisibleElement');
-    }
-    // console.log(`width: ${width}, height: ${height}`);
-}
-invisibleElement.classList.add('invisibleElement');
-window.addEventListener('resize', removeInvisibleElement);
 
 
