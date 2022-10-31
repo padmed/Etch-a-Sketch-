b3c86968795a8998afe0d3a8d045f7e758a781ae
padmed <ddarchinian@gmail.com>
2022-10-31 14:24:46 +0400

    Combine 2 classes for better readability, Add listener to colors

--- logic.js
+++ logic.js
@@ -2,21 +2,26 @@
 // 2. make a method for building a grid in sketchpad (done)
 // 3. add listener for grids in sketchpad (done)
 // 4. make a method for coloring grids in sketchpad
+    // 1. be able to choose color
 // 5. make a method to be able to clear sketchpad (method implemented/ need to listen to button)
 // 6. make option for grid option turn on/off
 // 7. make option to draw with hover or click
-
+//                                                                  learn more about bind method
 
 class EtchASketch {
     constructor() {
         this.pad = document.getElementById('draw-box');
         this.colorList = document.querySelectorAll('.color');
+        this.inputRange = document.getElementById('range');
+        this.invisibleElement = document.getElementById('invisibleElement');
         this.padSize = 16;
         this.color = 'black';
+
+        this.main();
     }
 
     //creates parent - child div elements, takes grid form with css flexbox 
-    fillPad = function (){
+    fillPad = function () {
         for (let height = 1; height <= this.padSize; height++) {
             const div = document.createElement('div');
             div.classList.add('parentDiv');
@@ -35,28 +40,16 @@ class EtchASketch {
         const toRemove = document.querySelectorAll('.parentDiv');
         toRemove.forEach((element) => element.remove());
     }
-}
-
-
-
-class EtchASketchMain {
-    constructor () {
-        this.pad = new EtchASketch;
-        this.inputRange = document.getElementById('range');
-        this.invisibleElement = document.getElementById('invisibleElement');
-        
-        this.main();
-    }
 
     //sets propertie based on user selection, clears previous grid, fills new grid.
-    interactiveInputRange = function () {
-        this.pad.padSize = this.inputRange.value;
-        this.pad.clearPad();
-        this.pad.fillPad();
+    #interactiveInputRange = function () {
+        this.padSize = this.inputRange.value;
+        this.clearPad();
+        this.fillPad();
     }
 
     // removes/adds invisible element based on window size
-    manipulateInvisibleElement = function () {
+    #manipulateInvisibleElement = function () {
         const width = document.documentElement.clientWidth;
         const height = document.documentElement.clientHeight;
 
@@ -66,18 +59,24 @@ class EtchASketchMain {
             invisibleElement.classList.add('invisibleElement');
         }
     }
-        
+
+    #takeColor = function (event) {
+        this.color = event.target.id
+        console.log(this.color)
+    }
 
     main = function () {
-        this.inputRange.addEventListener('input', this.interactiveInputRange.bind(this)); // listens to input 'range'
+        this.inputRange.addEventListener('input', this.#interactiveInputRange.bind(this)); // listens to input 'range'
         invisibleElement.classList.add('invisibleElement'); //adds invisible element on program startup
-        window.addEventListener('resize', this.manipulateInvisibleElement) // listens for window resize
+        window.addEventListener('resize', this.#manipulateInvisibleElement); // listens for window resize
+        this.colorList.forEach((colorDiv) => colorDiv.addEventListener('click', this.#takeColor.bind(this)));
     }
+
 }
 
 
 
-program = new EtchASketchMain()
 
 
+const pad = new EtchASketch();
 
